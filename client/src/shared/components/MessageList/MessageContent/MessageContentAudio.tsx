138805e45e6959569ca87470ac97f2b60db9c654
Nels0nMuntz <tchekerdes.vgen@ukr.net>
2022-01-17 23:00:56 +0200

    working on audio message markup

--- client/src/shared/components/MessageList/MessageContent/MessageContentAudio.tsx
+++ client/src/shared/components/MessageList/MessageContent/MessageContentAudio.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React from 'react';
 
-const MessageContentAudio: React.FC = () => {
+import PlayIcon from '@material-ui/icons/PlayArrow';
+
+
+
+type MessageContentAudioProps = {
+    selectMode: boolean;
+    meta?: string | JSX.Element;
+    handlePlay: () => void;
+    handleStop: () => void;
+    handleMessageClick: () => void;
+};
+
+const MessageContentAudio: React.FC<MessageContentAudioProps> = (props) => {
+
+    const {
+        selectMode,
+        meta,
+        handlePlay,
+        handleStop,
+        handleMessageClick,
+    } = props;
+
     return (
-        <div className="message-item__content message-item__content_audio">
-            <div className="audio-control">
-                <div className="audio-control__btn"></div>
+        <div className="message-item__content audio-message-item">
+            <div className="audio-message-item__inner">
+                <div className="audio-control">
+                    <button className="audio-control__btn" type="button" title="Play audio">
+                        <span className="visually-hidden">Play audio</span>
+                        <PlayIcon />
+                    </button>
+                    <div className="audio-control__content audio-control__content_unread">
+                        <div className="audio-control__waveform"></div>
+                        <div className="audio-control__duration">0:03</div>
+                    </div>
+                </div>
+                {
+                    meta
+                        ? <span 
+                            className="message-text-content__meta"
+                            onClick={handleMessageClick}
+                        >
+                            {meta}
+                        </span>
+                        : null
+                } 
             </div>
         </div>
     )
