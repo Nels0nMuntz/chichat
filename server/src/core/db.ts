d947eaca10063365acfc5cba2d03c067424f0a48
Nels0nMuntz <tchekerdes.vgen@ukr.net>
2023-10-03 19:26:00 +0300

    updated connection db string

--- server/src/core/db.ts
+++ server/src/core/db.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
+    const { MONGO_CONNECTION_STRING } = process.env;
     try {
         const db = mongoose.connection;
         db.on("open", () => console.log("Successfully connected to MongoDB"));
         db.on("error", () => console.log("Connection to MongoDB faild"));
-        await mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+        await mongoose.connect(MONGO_CONNECTION_STRING);
     } catch (error) {
         console.log(error);
         process.exit(1);
